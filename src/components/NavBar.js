8b0e9370e8c8ee69addaecd674e830511a99a8ca
oli-mac <adumulugeta21@gmail.com>
2023-04-06 13:19:11 +0300

    setup Logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
+import Logo from './Logo'
+
 
 const NavBar = () => {
   return (
     <div
-    className='w-full px-32 py-8 font-medium'
+    className='w-full px-32 py-8 font-medium flex items-center justify-between'
     >
         <nav>
             <Link href="/"> Home </Link>
@@ -12,8 +14,20 @@ const NavBar = () => {
             <Link href="/projects"> Projects </Link>
             <Link href="/articles"> Articles </Link>
         </nav>
-        <h2>logo</h2>
+        {/* <Logo /> */}
+        <nav>
+            <Link href="/" target={'_blank'}>T</Link>
+            <Link href="/" target={'_blank'}>T</Link>
+            <Link href="/" target={'_blank'}>T</Link>
+            <Link href="/" target={'_blank'}>T</Link>
+            <Link href="/" target={'_blank'}>T</Link>
+            <Link href="/" target={'_blank'}>T</Link>
+        </nav>
         
+        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+            <Logo />
+        </div>
+
     </div>
   )
 }
