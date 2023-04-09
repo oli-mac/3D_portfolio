306818d60ea843d606c289ea1b9bed87ad2dafea
oli-mac <adumulugeta21@gmail.com>
2023-04-10 00:29:33 +0300

    Animate Text

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,10 +11,6 @@ export default function App({ Component, pageProps }) {
   return (
 
     <>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
      <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
       <NavBar />
       <Component {...pageProps} />
