430ff8479cc4e3a2ea2e14614549d88b4e7112a5
oli-mac <adumulugeta21@gmail.com>
2023-04-10 00:48:35 +0300

    Animate Text

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import profilePic from "../../public/images/profile/developer-pic-1.png"
 import AnimatedText from '@/components/AnimatedText'
+import Link from 'next/link'
+import { LinkArrow } from '@/components/Icon'
 
 
 export default function Home() {
@@ -22,10 +24,18 @@ export default function Home() {
             </div>
             <div className='w-1/2 flex flex-col items-center self-center'>
               <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left'/>
-              <p>
+              <p className='my-4 text-base font-medium'>
                   As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
                   Explore my latest projects and articles, showcasing my expertise in React.js and web development.
               </p>
+              <div className=' flex items-center self-start mt-2'>
+                <Link className='flex items-center bg-dark text-light p-2.5 px-6 
+                rounded-lg text-lg font-semibold hover:bg-light hover:text-dark 
+                border-2 border-solid border-transparent hover:border-dark' href="/dummy.pdf" target='_blank' download={true}>
+                  Resume<LinkArrow className="w-6 ml-1"/>
+                  </Link>
+                <Link className='ml-4 text-lg font-medium capitalize text-dark underline' href="mailto:contactolyad@gmail.com" target='_blank'>Contact</Link>
+              </div>
             </div>
           </div>
 
