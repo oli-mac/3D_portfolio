dfe3bce043097d67e2485a78197dc563e3485b5c
oli-mac <adumulugeta21@gmail.com>
2023-04-11 02:05:39 +0300

    About Me Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,38 @@ import AnimatedText from '@/components/AnimatedText'
 import Layout from '@/components/Layout'
 import Image from 'next/image'
 import Head from 'next/head'
-import React from 'react'
+import React, { useRef } from 'react'
 import ProfilePic from "../../public/images/profile/olyad.png"
+import { useInView, useMotionValue, useSpring } from 'framer-motion'
+import { useEffect } from 'react'
+
+
+const AnimatedNumbers = ({value}) =>{
+const ref = useRef(null)
+
+const motionValue = useMotionValue(0);
+const springValue = useSpring(motionValue, { duration: 3000 });
+const isInView = useInView(ref);
+
+useEffect(() => {
+  if(isInView){
+    motionValue.set(value);
+  }
+}, [isInView, value, motionValue])
+
+useEffect(() => {
+    springValue.on("change", (latest) => {
+        if(ref.current && latest.toFixed(0) <= value){
+            ref.current.textContent = latest.toFixed(0);
+        }
+    })
+  
+}, [ springValue, value])
+
+
+    return <span ref={ref}></span>
+}
+
 
 const About = () => {
   return (
@@ -12,7 +42,7 @@ const About = () => {
             <title>Olyad Mulugeta| About Page</title>
             <meta name="description" content="About me" />
         </Head>
-        <main className='flex w-full flex-col items-center justify-center'>
+        <main className='flex w-full flex-col items-center justify-center mb-16'>
             <Layout className='pt-16'>
                  <AnimatedText text='Passion Fuels Purpose! ' className='mb-16'/>
                 <div className='grid w-full grid-cols-8 gap-16'>
@@ -34,8 +64,23 @@ const About = () => {
                         </p>
                     </div>
                     <div className=' col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
-                        <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark'/>
+                        <div className='absolute top-0 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark'/>
                         <Image src={ProfilePic} alt="olyad Mulugeta" className="w-full h-auto rounded-2xl"/>
+                    </div>
+                    <div className="col-span-2 flex flex-col items-end justify-between mr-16">
+                       
+                        <div className='flex flex-col items-end justify-center'>
+                            <span className='inline-block text-7xl font-bold'><AnimatedNumbers value={10}/>+</span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75'>Satisfied clients</h2>
+                        </div>
+                        <div className='flex flex-col items-end justify-center'>
+                            <span className='inline-block text-7xl font-bold'><AnimatedNumbers value={15}/>+</span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75'>Projects Completed</h2>
+                        </div>
+                        <div className='flex flex-col items-end justify-center'>
+                            <span className='inline-block text-7xl font-bold'><AnimatedNumbers value={2}/>+</span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75'>Yeard Of Experience</h2>
+                        </div>
                     </div>
                 </div>
             </Layout>
