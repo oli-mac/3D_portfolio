6e251a902bfe7c9c7117be9c345291fbd7f1a595
oli-mac <adumulugeta21@gmail.com>
2023-04-11 00:42:57 +0300

    About Me Page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,7 +35,7 @@ const NavBar = () => {
     >
         <nav>
             <CustomLink href="/" title="Home " className='mr-4'/> 
-            <CustomLink href="/about" title="About " className='mx-4'/> 
+            <CustomLink href="/About" title="About " className='mx-4'/> 
             <CustomLink href="/projects" title="Projects " className='mx-4'/> 
             <CustomLink href="/articles" title="Articles " className='ml-4'/> 
         </nav>
---
+++ src/pages/About.js
@@ -0,0 +1,47 @@
+import AnimatedText from '@/components/AnimatedText'
+import Layout from '@/components/Layout'
+import Image from 'next/image'
+import Head from 'next/head'
+import React from 'react'
+import ProfilePic from "../../public/images/profile/olyad.png"
+
+const About = () => {
+  return (
+    <>
+        <Head>
+            <title>Olyad Mulugeta| About Page</title>
+            <meta name="description" content="About me" />
+        </Head>
+        <main className='flex w-full flex-col items-center justify-center'>
+            <Layout className='pt-16'>
+                 <AnimatedText text='Passion Fuels Purpose! ' className='mb-16'/>
+                <div className='grid w-full grid-cols-8 gap-16'>
+                    <div className='col-span-3 flex ml-16 flex-col items-start justify-start'>
+                        <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'> About Me</h2>
+                        <p className='font-medium'>
+                            Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
+                            and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
+                            new and innovative ways to bring my clients' visions to life.
+                        </p>
+                        <p className='my-4 font-medium'>
+                            I believe that design is about more than just making things look pretty – it's about solving problems and 
+                            creating intuitive, enjoyable experiences for users.        
+                        </p>
+                        <p className='font-medium'>
+                            Whether I'm working on a website, mobile app, or 
+                            other digital product, I bring my commitment to design excellence and user-centered thinking to 
+                            every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
+                        </p>
+                    </div>
+                    <div className=' col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
+                        <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark'/>
+                        <Image src={ProfilePic} alt="olyad Mulugeta" className="w-full h-auto rounded-2xl"/>
+                    </div>
+                </div>
+            </Layout>
+        </main>
+    </>
+  )
+}
+
+export default About
