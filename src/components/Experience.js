b27d1d9d2c837b93a636c6951695a89400ea8434
oli-mac <adumulugeta21@gmail.com>
2023-04-11 18:14:30 +0300

    Experience Component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,16 +1,23 @@
 import React, { useRef } from 'react'
 import { useScroll } from 'framer-motion'
 import { motion } from 'framer-motion'
+import LiIcon from './LiIcon'
 
 const Details = ({position, company, companyLink, time, address, work}) =>{
-    return <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
-        <div>
+    const ref = useRef(null);
+    return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+        <LiIcon reference={ref}/>
+        <motion.div
+        initial={{y:50}}
+        whileInView={{y:0}}
+        transition={{duration:0.5, type:"spring"}}
+        >
             <h3 className='capitalize font-bold text-2xl'>{position}&nbsp; <a className='text-primary capitalize' href={companyLink} target='_blank'>@{company}</a></h3>
             <span  className='capitalize font-medium text-dark/75'>{time} | {address}</span>
             <p className='font-medium w-full'>
                 {work}
             </p>
-        </div>
+        </motion.div>
     </li>
 }
 
@@ -29,7 +36,7 @@ const Experience = () => {
         <h2 className='font-bold text-8xl mb-32 w-full text-center'>Experience</h2>
         
         <div ref={ref} className='w-[75%] mx-auto relative'>
-            <motion.div style={{scaleY: scrollYProgress}} className='absolute left-8 top-0 w-[4px] h-full bg-dark origin-top'/>
+            <motion.div style={{scaleY: scrollYProgress}} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top'/>
 
             <ul className='w-full flex flex-col items-start justify-between ml-4'>
                 <Details 
