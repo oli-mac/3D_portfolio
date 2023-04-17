6883f0c7f34de50af4ff3e8ff537a5285ddb1e47
oli-mac <adumulugeta21@gmail.com>
2023-04-18 01:35:20 +0300

    responsiveness

--- src/pages/About.js
+++ src/pages/About.js
@@ -46,13 +46,14 @@ const About = () => {
             <meta name="description" content="About me" />
         </Head>
         <main className='flex w-full flex-col items-center justify-center mb-16 dark:text-light'>
-            <Layout className='pt-16 px-0'>
-                 <AnimatedText text='Passion Fuels Purpose! ' className='mb-16'/>
-                <div className='grid w-full grid-cols-8 gap-16'>
-                    <div className='col-span-3 flex ml-16 flex-col items-start justify-start'>
+            <Layout className='pt-16 '>
+                 <AnimatedText text='Passion Fuels Purpose! ' className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8'/>
+                <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+                    <div className='col-span-3 flex ml-4 flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
                         <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light' > About Me</h2>
                         <p className='font-medium'>
-                            Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
+                            {/* ===================================================== */}
+                            Hi, I'm Olyad Mulugeta, a web developer, UI/UX designer and Software Engener with a passion for creating beautiful, functional, 
                             and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
                             new and innovative ways to bring my clients' visions to life.
                         </p>
@@ -66,7 +67,8 @@ const About = () => {
                             every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
                         </p>
                     </div>
-                    <div className=' col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
+                    <div className=' col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light
+                    xl:col-span-4 md:order-1 md:col-span-8'>
                         <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light'/>
                         <Image src={ProfilePic} alt="olyad Mulugeta" className="w-full h-auto rounded-2xl"
                         priority
@@ -74,19 +76,19 @@ const About = () => {
                         (max-width:1200px) 50vw,33vw
                         '/>
                     </div>
-                    <div className="col-span-2 flex flex-col items-end justify-between mr-16">
+                    <div className="col-span-2 flex flex-col items-end justify-between mr-16 xl:col-span-8 xl:flex-row xl:items-center md:order-3">
                        
-                        <div className='flex flex-col items-end justify-center'>
-                            <span className='inline-block text-7xl font-bold'><AnimatedNumbers value={10}/>+</span>
-                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Satisfied clients</h2>
+                        <div className='flex flex-col items-end justify-center xl:items-center'>
+                            <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'><AnimatedNumbers value={10}/>+</span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Satisfied clients</h2>
                         </div>
-                        <div className='flex flex-col items-end justify-center'>
-                            <span className='inline-block text-7xl font-bold'><AnimatedNumbers value={15}/>+</span>
-                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Projects Completed</h2>
+                        <div className='flex flex-col items-end justify-center xl:items-center'>
+                            <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'><AnimatedNumbers value={15}/>+</span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Projects Completed</h2>
                         </div>
-                        <div className='flex flex-col items-end justify-center'>
-                            <span className='inline-block text-7xl font-bold'><AnimatedNumbers value={2}/>+</span>
-                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Yeard Of Experience</h2>
+                        <div className='flex flex-col items-end justify-center xl:items-center'>
+                            <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'><AnimatedNumbers value={2}/>+</span>
+                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Yeard Of Experience</h2>
                         </div>
                     </div>
                 </div>
