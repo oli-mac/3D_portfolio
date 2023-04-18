017f3c19c58ed9033aec9f8c1d8d410955e1ca27
oli-mac <adumulugeta21@gmail.com>
2023-04-18 11:14:36 +0300

    transition effect

---
+++ src/components/TransitionEffect.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { motion, useMotionValue } from 'framer-motion'
+
+
+const TransitionEffect = () => {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default TransitionEffect
